ed4b5aefe1a229335438e652f6bd1cca18e7757a
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-26 22:31:38 +0530

    Added: Player Money And Stake

--- src/com/bridglabz/GamblingGame.java
+++ src/com/bridglabz/GamblingGame.java
@@ -1,8 +1,14 @@
 package com.bridglabz;
 
+import java.util.Random;
+
 public class GamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Game");
+        int money=100;
+        int betMoney=1;
+        System.out.println("start with a stake of " + money +" every day and bet " + betMoney +" every game.");
+    }
     }
 
-}
+
