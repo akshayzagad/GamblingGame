9792d67feefd81e63f6e7e8ac1bdee18220ecaa2
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-26 22:53:07 +0530

    Added: Player is Win or Loss

--- src/com/bridglabz/GamblingGame.java
+++ src/com/bridglabz/GamblingGame.java
@@ -7,8 +7,16 @@ public class GamblingGame {
         System.out.println("Welcome to Gambling Game");
         int money=100;
         int betMoney=1;
+        Random random = new Random();
         System.out.println("start with a stake of " + money +" every day and bet " + betMoney +" every game.");
+        int bet =random.nextInt(2);
+            if (bet==1){
+                System.out.println("Player is Win the bet");
+            }else {
+                System.out.println("Player is the loss bet");
+            }
+        }
     }
-    }
+
 
 
